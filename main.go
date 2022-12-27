71d8cbfa0860f66e229564613b2745b09778c356
Muhammad Rizky Amrullah <rizkii.amr@gmail.com>
2022-12-28 02:28:08 +0700

    feat: change / endpoint to /ruok

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"encoding/json"
 
 	"github.com/rizkiamr/x-response-time/httptimer"
 )
 
 func main() {
 	h := http.NewServeMux()
-	h.HandleFunc("/", index)
+	h.HandleFunc("/ruok", index)
 
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", httptimer.Timed(h)))
 }
@@ -23,7 +24,12 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 	time.Sleep(wait)
 
-	w.Header().Set("Content-Type", "text-plain")
-	fmt.Fprintf(w, "%v: %v\n", r.Method, r.URL.Path)
-	fmt.Fprintln(w, "Check X-Response-Time for the processing time of this request.")
+	w.Header().Set("Content-Type", "application/json")
+	//fmt.Fprintf(w, "%v: %v\n", r.Method, r.URL.Path)
+	//fmt.Fprintln(w, "Check X-Response-Time for the processing time of this request.")
+
+	resp := make(map[string]string)
+	resp["message"] = "ok"
+	jsonResp, _ := json.Marshal(resp)
+	w.Write(jsonResp)
 }
